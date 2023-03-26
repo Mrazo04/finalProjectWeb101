ac92e0e9fe90c72e35cd89e2e9f3106e348c9c2d
Mrazo04 <munguiamickey@gmail.com>
2023-03-26 18:29:08 -0500

    added 992px media query

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const mediaQuery = window.matchMedia('(min-width: 992px')
-
-const mediaQuery = window.matchMedia('(min-width: 1200px')
-
 window.onscroll = function () {
   scrollRotate();
 };
 
 function scrollRotate() {
   let image = document.getElementById("circle");
-  image.style.transform = "rotate("+ window.pageYOffset + "deg)";
+  image.style.transform = "rotate("+ window.pageYOffset/3 + "deg)";
 }
